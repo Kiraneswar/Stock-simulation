4f775635d52f3ff956969a760a14a5476a0ac663
Kiraneswar <kiraneswar999@gmail.com>
2026-04-22 20:17:15 +0530

    commited

--- backend/models/Transaction.js
+++ backend/models/Transaction.js
@@ -5,7 +5,7 @@ const TransactionSchema = new mongoose.Schema({
     stockId: { type: mongoose.Schema.Types.ObjectId, ref: 'Stock', required: true },
     type: { type: String, enum: ['buy', 'sell'], required: true },
     quantity: { type: Number, required: true },
-    price: { type: Number, required: true }, // Price at the time of transaction
+    price: { type: Number, required: true },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Transaction', TransactionSchema);
